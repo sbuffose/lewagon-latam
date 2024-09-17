3354b457eb5e20ce42836e7e613eaa995dd8b105
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2024-09-17 13:36:39 -0300

    add validation

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,3 +1,5 @@
 class Lead < ApplicationRecord
-  validates :first_name, :last_name, :email
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
